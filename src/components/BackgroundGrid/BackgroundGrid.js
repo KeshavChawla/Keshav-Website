5dbbf84210e324eb6cbb33352365a3a4d8e2b8e2
Keshav Chawla <keshavchawla2014@gmail.com>
2021-03-18 22:48:44 -0400

    feature: svg animation

--- src/components/BackgroundGrid/BackgroundGrid.js
+++ src/components/BackgroundGrid/BackgroundGrid.js
@@ -3,6 +3,10 @@ import React, { memo, useEffect, useRef } from 'react';
 import checkProps from '@jam3/react-check-extra-props';
 import { gsap } from 'gsap';
 
+import Vivus from 'vivus';
+import KeshavFirstNameSVG from '../KeshavFirstNameSVG/KeshavFirstNameSVG';
+// import KeshavFirstNameSVG from '../../assets/svgs/KeshavName_animated.svg';
+
 import styles from './BackgroundGrid.module.scss';
 
 // import routes from '../../data/routes';
@@ -10,52 +14,66 @@ import styles from './BackgroundGrid.module.scss';
 function BackgroundGrid() {
   //   const gridLineRef = useRef(null);
   const gridRef = useRef(null);
+  const firstNameRef = useRef(null);
+
   //   var verticalLines = [];
   const itemEls = useRef([]);
   const itemElsHoriz = useRef([]);
 
   useEffect(() => {
-    const animationDuration = 5;
-    const staggerAmount = 2;
-    const secondStartTime = 5;
+    // new Vivus(firstNameRef.current, { duration: 200, file: KeshavFirstNameSVG });
+
+    // const animationDuration = 5;
+    // const staggerAmount = 2;
+    // const secondStartTime = 5;
 
     gsap
       .timeline({ defaults: { stagger: 0.1, duration: 2, delay: 0 } })
       .from(gridRef.current, { autoAlpha: 0, duration: 0.3 }, 0)
       .from(itemEls.current, { scaleY: 0, transformOrigin: '0% 0%', duration: 2, stagger: 0.05 }, 0.3)
       .from(itemElsHoriz.current, { scaleX: 0, transformOrigin: '0% 0%' }, 0.3);
-    gsap
-      .timeline({
-        delay: 2,
-        repeat: -1,
-        defaults: { stagger: staggerAmount, duration: animationDuration, delay: 0 }
-      })
-      .to(itemEls.current, { scaleY: 0, transformOrigin: '0% 100%' }, 0)
-      .to(itemElsHoriz.current, { scaleX: 0, transformOrigin: '100% 0%' }, 0)
-      .from(itemEls.current, { scaleY: 0, transformOrigin: '0% 0%' }, secondStartTime)
-      .from(itemElsHoriz.current, { scaleX: 0, transformOrigin: '0% 0%' }, secondStartTime);
+
+    // gsap
+    //   .timeline({
+    //     delay: 2,
+    //     repeat: -1,
+    //     defaults: { stagger: staggerAmount, duration: animationDuration, delay: 0 }
+    //   })
+    //   .to(itemEls.current, { scaleY: 0, transformOrigin: '0% 100%' }, 0)
+    //   .to(itemElsHoriz.current, { scaleX: 0, transformOrigin: '100% 0%' }, 0)
+    //   .from(itemEls.current, { scaleY: 0, transformOrigin: '0% 0%' }, secondStartTime)
+    //   .from(itemElsHoriz.current, { scaleX: 0, transformOrigin: '0% 0%' }, secondStartTime);
+    console.log(firstNameRef.current);
+    new Vivus(firstNameRef.current, { duration: 200 }, null);
   }, [itemEls, itemElsHoriz]);
 
   const numberOfLines = 40;
   return (
-    <div ref={gridRef} className={styles.backgroundGrid}>
-      {/* <div ref={gridLineRef} className={styles.gridLine} /> */}
-      <div className={styles.vertDivContainer}>
-        {[...Array(numberOfLines)].map((x, i) => (
-          <div ref={(element) => itemEls.current.push(element)} className={styles.gridLine} key={'verticalLine' + i} />
-        ))}
-      </div>
+    <>
+      <div ref={gridRef} className={styles.backgroundGrid}>
+        <KeshavFirstNameSVG ref={firstNameRef} />
+        {/* <div ref={gridLineRef} className={styles.gridLine} /> */}
+        <div className={styles.vertDivContainer}>
+          {[...Array(numberOfLines)].map((x, i) => (
+            <div
+              ref={(element) => itemEls.current.push(element)}
+              className={styles.gridLine}
+              key={'verticalLine' + i}
+            />
+          ))}
+        </div>
 
-      <div className={styles.horizDivContainer}>
-        {[...Array(numberOfLines)].map((x, i) => (
-          <div
-            ref={(element) => itemElsHoriz.current.push(element)}
-            className={styles.gridLineHoriz}
-            key={'horiztonalLine' + i}
-          />
-        ))}
+        <div className={styles.horizDivContainer}>
+          {[...Array(numberOfLines)].map((x, i) => (
+            <div
+              ref={(element) => itemElsHoriz.current.push(element)}
+              className={styles.gridLineHoriz}
+              key={'horiztonalLine' + i}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
